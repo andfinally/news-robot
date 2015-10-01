90e8b379b7c969beee84b6a5476338165f74f8c3
And Finally <andfinally@users.noreply.github.com>
2015-10-01 15:49:55 +0100

    Page with countdown and auto-reload.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,22 @@
 
 // http://testapi.metro.co.uk/twitter/articles.json?callback=ajaxfun
 
-var app = angular.module('twitterServiceApp', []);
+var app = angular.module('twitterServiceApp', ['ngRoute']);
 
-app.controller('TweetListCtrl', function ($scope, $http) {
-	$http.get('js/test-data.json').success(function(data){
-		$scope.tweets = data;
-	});
-});
+app.config(['$routeProvider',
+	function ($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: 'partials/tweet-list.html',
+				controller : 'TweetListCtrl'
+			})
+	}
+]);
+
+app.controller('TweetListCtrl', ['$scope', '$http', function ($scope, $http) {
+	var url = 'http://testapi.metro.co.uk/twitter/articles.json?callback=JSON_CALLBACK';
+	$http.jsonp(url).
+		success(function (data, status) {
+			$scope.tweets = data;
+		});
+}]);
