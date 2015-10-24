787b2827ac8b3301b188ed7be340bca108886660
andfinally <andfinally@users.noreply.github.com>
2015-10-24 10:12:28 +0100

    Added console log to server.js.

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ var app = express();
 app.use(express.static('.'));
 
 app.listen( process.env.PORT || 3000);
+
+console.log('');
+console.log('=========================================');
+console.log('Server listening at http://localhost:3000');
+console.log('=========================================');
+console.log('');
